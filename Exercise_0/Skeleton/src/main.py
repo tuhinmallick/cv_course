ccc8a4e0c8b35fd60ba33d688b6b860f700ee0ca
eb88ibyw <soroosh.arasteh@fau.de>
2020-04-24 17:37:21 +0200

    initial commit

---
+++ Exercise_0/Skeleton/src/main.py
@@ -0,0 +1,34 @@
+import numpy
+import cv2
+
+## TODO 3.1
+## Load Image
+## Show it on screen
+file = '../img.png' ## path to the image
+'''
+Your code here
+'''
+
+
+
+###############################################
+## TODO 3.2
+## Resize Image by a factor of 0.5
+## Show it on screen
+## Save as small.jpg
+'''
+Your code here
+'''
+
+
+###############################################
+## TODO 3.3
+## Create and save 3 single-channel images from small image
+## one image each channel (r, g, b)
+## Display the channel-images on screen
+'''
+Your code here
+'''
+
+
+###############################################
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # CV_course
 
+This project contains programming exercises of the Computer Vision course (SS20) offered by the Pattern Recognition Lab (LME) of the Computer Science Department at University of Erlangen-Nuremberg (FAU).
+
